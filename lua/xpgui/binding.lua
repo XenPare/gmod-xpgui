051bc5c4c27694e081ed7feff8621c197db67b8d
crester <35332822+cresterienvogel@users.noreply.github.com>
2022-11-12 03:18:58 +0700

    updated closing logic

--- lua/xpgui/binding.lua
+++ lua/xpgui/binding.lua
@@ -9,7 +9,13 @@ function XPGUI.GetFirst()
 end
 
 function XPGUI.GetAmount()
-	return #XPGUI.Opened
+	local amount = 0
+	for _, pnl in ipairs(XPGUI.Opened) do
+		if IsValid(pnl) and pnl:IsVisible() then
+			amount = amount + 1
+		end
+	end
+	return amount
 end
 
 function XPGUI.RemoveLast()
@@ -28,7 +34,7 @@ end
 
 local firstPressed
 hook.Add("PreRender", "XPGUI Binding", function()
-	if not firstPressed and input.IsButtonDown(KEY_ESCAPE) and #XPGUI.Opened > 0 then
+	if not firstPressed and input.IsButtonDown(KEY_ESCAPE) and XPGUI.GetAmount() > 0 then
 		XPGUI.RemoveLast()
 		firstPressed = true
 		if gui.IsGameUIVisible() then
